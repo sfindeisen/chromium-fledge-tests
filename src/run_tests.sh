acea2f1062767917edc424c29b5b19723607b5b5
Stanislaw Findeisen <stanislaw@ik.me>
2024-06-25 12:07:14 +0200

    ML-407 create a docker volume for test logs

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -5,9 +5,15 @@ echo "(1) hello from chromium-fledge-tests/src/run_tests.sh; PYTHONPATH=$PYTHONP
 pwd
 ls -la
 
+echo "(2) hello from chromium-fledge-tests/src/run_tests.sh; PYTHONPATH=$PYTHONPATH"
+ls -la ..
+
+echo "(3) hello from chromium-fledge-tests/src/run_tests.sh; PYTHONPATH=$PYTHONPATH"
+ls -la ../tests-log
+
 if [[ -z TEST_LIB_DIR ]] && [[ -n "${TEST_LIB_DIR}" ]]; then
   export PYTHONPATH="${TEST_LIB_DIR}:${PYTHONPATH}"
 fi
 
-echo "(2) hello from chromium-fledge-tests/src/run_tests.sh; PYTHONPATH=$PYTHONPATH"
+echo "(4) hello from chromium-fledge-tests/src/run_tests.sh; PYTHONPATH=$PYTHONPATH"
 python3 -m unittest $TEST --verbose
